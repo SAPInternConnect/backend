49f44bb6a043866508ced456f243b902dceca6a0
kwong <kirkwong33@gmail.com>
2020-10-23 21:29:47 -0700

    Adding login/registration/auth

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	apiKey: 'AIzaSyBs63jFHVnh37ARQ6kAGPmfHXaLY_pz6dY',
+	authDomain: 'sapconnect-42519.firebaseapp.com',
+	databaseURL: 'https://sapconnect-42519.firebaseio.com',
+	projectId: 'sapconnect-42519',
+	storageBucket: 'sapconnect-42519.appspot.com',
+	messagingSenderId: '324428614659',
+	appId: '1:324428614659:web:7c0a2e12049b32e203d236',
+};
